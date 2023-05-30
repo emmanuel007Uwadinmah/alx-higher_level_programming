927fa03e9f7cb3105ea8969017caf86ea28def4b
emmanuel007uwadinmah <emmanueluwadinmah@gmail.com>
2023-05-30 06:03:52 -0700

    0-square.py completed

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """An empty square class"""
+    pass
+
+if __name__ == '__main__':
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
