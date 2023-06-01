6d31f94a0aca859bf9bf9a3f6b7804f29eece776
emmanuel007uwadinmah <emmanueluwadinmah@gmail.com>
2023-06-01 11:16:31 +0100

    Task completed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-class Square:
-    """An empty square class"""
-    pass
+""" Creating a square class """
 
-if __name__ == '__main__':
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
+
+class Square:
+    """ Creating a square class """
+    pass
