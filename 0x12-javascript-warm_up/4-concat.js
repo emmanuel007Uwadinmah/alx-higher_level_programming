c006e5ade19d5eb19aa35978222078465c831288
emmanuel007uwadinmah <emmanueluwadinmah@gmail.com>
2023-11-13 17:45:24 +0100

    Task completed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-
-if (args.length >= 2) {
-  console.log(args[0] + ' is ' + args[1]);
-} else {
-  console.log('Not enough arguments provided');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
