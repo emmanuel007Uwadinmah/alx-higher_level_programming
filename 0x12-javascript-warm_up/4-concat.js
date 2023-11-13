48716411d91ce30e2b280fcc9079a5d13be442c9
root <emmanueluwadinmah@gmail.com>
2023-11-13 06:48:17 -0800

    Task completed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 
 if (args.length >= 2) {
-  console.log(`${args[0]} is ${args[1]}`);
+  console.log(args[0] + ' is ' + args[1]);
 } else {
   console.log('Not enough arguments provided');
 }
